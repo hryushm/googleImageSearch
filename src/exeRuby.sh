0a6fdd1695d0cc86bbe895abaad8c026010cae1c
kuroshichimi <kuroshichimi03@gmail.com>
2016-05-14 00:28:10 +0900

    不要なファイルを削除。setup.sh修正

--- src/exeRuby.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export API_KEY=`cat ../secrets/apikey`
-export SEARCH_ENGINE_ID=`cat ../secrets/id`
-ruby $1 $2
--- src/setENV.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export API_KEY=`cat apikey.txt`
-export SEARCH_ENGINE_ID=`cat id.txt`
